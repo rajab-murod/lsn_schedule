a792cfc4680c6065c55c6435502f240f91614bf2
rajab-murod <mr.header1@gmail.com>
2024-12-05 18:12:43 +0500

    test_user worked

--- main/tests/test_user.py
+++ main/tests/test_user.py
@@ -1,22 +1,19 @@
 import pytest
 from rest_framework.authtoken.models import Token
-import logging
-
-logger = logging.getLogger(__name__)
 
 
+@pytest.mark.django_db
 def test_login(api_client, user):
    payload = {
-      'username': user.username,
-      'password': user.password
+      'username': "testuser",
+      'password': "securepassword",
    }
 
    res = api_client.post('/api/login/', data=payload, format='json')
-   logger.info("INFO:", res.data)
    assert res.status_code == 200
-   # assert res.data['id'] == user.id
-   assert "token" in res.data  # The response should include the token
-   # token = res.data["token"]
-   # assert Token.objects.filter(key=token).exists()
+   assert res.data['id'] == user.id
+   assert "auth_token" in res.data
+   token = res.data["auth_token"]
+   assert Token.objects.filter(key=token).exists()
 
 
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework.authtoken.views import obtain_auth_token
 
 from main.views import LessonScheduleViewSet \
    , StudyPlanViewSet, EduYearViewSet, AddEmployeeAPIView, LoginView, LogOutView
@@ -26,8 +25,7 @@ schema_view = get_schema_view(
 
 urlpatterns += [
    path('add/employee/', AddEmployeeAPIView.as_view(), name='add-employee'),
-   # path('login/', LoginView.as_view(), name='login'),
-   path('login/', obtain_auth_token, name='api_token_auth'),
+   path('login/', LoginView.as_view(), name='login'),
    path('logout/', LogOutView.as_view(), name='logout'),
    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
